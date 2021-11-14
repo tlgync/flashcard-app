d7080b8401465baa37b23b9abcf71ffa39bc1fe8
Tolga Yonca <ynctlg@gmail.com>
2021-11-15 01:14:22 +0300

    Update Progressbar.tsx

--- src/components/Progressbar.tsx
+++ src/components/Progressbar.tsx
@@ -7,7 +7,7 @@ export const Progressbar = ({ data }: any) => {
   const newNow = ((index + 1) * 100) / data.length;
   return (
     <div className="progressbar-container">
-      <ProgressBar striped variant="warning" now={newNow} style={{ width: 300, height: 9 }} />
+      <ProgressBar striped variant="warning" now={newNow} style={{ width: 275, height: 9 }} />
       <span>{`${index + 1} / ${data.length}`}</span>
     </div>
   );
